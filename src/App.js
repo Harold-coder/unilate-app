4345a36db4c5aadc73515889053741b0cbb27d97
Harold Castiaux <haroldcastiaux@Harolds-MacBook-Pro.local>
2023-07-22 18:05:14 -0400

    Added a search bar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Hero from './components/Hero';
 import Navbar from './components/Navbar';
+import Search from './components/Search';
 
 function App() {
   return (
     <div className='app'>
       <Navbar/>
       <Hero/>
+      <Search/>
     </div>
   );
 }
---
+++ src/components/Search.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+export default function Search() {
+    return (
+        <div className="input-area">
+            <input className="search" type="text" placeholder="Rechercher votre médecin"></input>
+        </div>
+    )
+}
