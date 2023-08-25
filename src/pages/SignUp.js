066e0cbadf2e97336b179d87c84d515cfc31165f
Harold Castiaux <haroldcastiaux@Harolds-MacBook-Pro.local>
2023-08-25 14:30:06 +0200

    Fixed the navigate issue and added some checks for the format

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,6 +1,6 @@
 import NavbarPatient from "../components/PatientPage/NavbarPatient";
-import { Link } from "react-router-dom";
-import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
 import Axios from "axios"
 
 function SignUp() {
@@ -11,7 +11,12 @@ function SignUp() {
   const [profession, setProfession] = useState()
   const [city, setCity] = useState()
   const [gender, setGender] = useState()
+
   const [passwordMatch, setPasswordMatch] = useState(true)
+  const [validFormat, setValidFormat] = useState(true)
+
+  const navigate = useNavigate();
+
   const createUser = (email, password, fullName, profession, city, gender) => {
     Axios.post("https://unilate-server-f22fc8c7c32c.herokuapp.com/createUser", {
       email: email,
@@ -23,16 +28,25 @@ function SignUp() {
     });
   }
 
-  function submitForm(){
-    // Check that passwords match
-    if (password !== confirmPassword){
+  function submitForm(event){
+    event.preventDefault();
+    console.log(!email)
+    if (!email || email.length < 5 || !fullName || fullName.length < 4 || !profession || profession.length < 2 || !city || city.length < 2){
+      setValidFormat(false);
+    } else if (!password || !confirmPassword || password !== confirmPassword || password.length < 3){
       setPasswordMatch(false);
     }
     else {
       setPasswordMatch(true);
       createUser(email, password, fullName, profession, city, gender);
+      navigate('/doctorPage')
     }
   }
+
+  useEffect(() => {
+    setPasswordMatch(true);
+    setValidFormat(true);
+  }, [email, fullName, city, profession, gender, password, confirmPassword])
   return (
     <div>
     <NavbarPatient/>
@@ -67,8 +81,9 @@ function SignUp() {
                 <label className="signup-label">Confirmer le mot de passe</label>
                 <input type="password" placeholder="*********" className="login-input" onChange={(e) => setConfirmPassword(e.target.value)}></input>
                 {!passwordMatch && <label className="signup-label signup-label-red">Le mot de passe ne match pas.</label>}
+                {!validFormat && <label className="signup-label signup-label-red">Some argument missing.</label>}
                 </div>
-                <Link className="link" to="/doctorPage"><button className="signup-button" onClick={submitForm}>Créer le compte</button></Link>
+                <button className="signup-button" onClick={submitForm}>Créer le compte</button>
             </form>
         </div>
     </div>
