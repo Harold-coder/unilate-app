c5c35b1f1e4c3ab3e2eb7f5d658df5345270bc69
Harold Castiaux <haroldcastiaux@Harolds-MacBook-Pro.local>
2023-08-26 14:00:53 +0200

    PatientPage now shows delay based on time

--- src/components/General/Retard.js
+++ src/components/General/Retard.js
@@ -4,9 +4,13 @@ import { useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
 
 export default function Retard(props) {
+    const date = new Date();
+    let hour = date.getHours();
     const [user, setUser] = useState()
     const [loaded, setLoaded] = useState(false)
     const [delay, setDelay] = useState()
+
+    const [patientHour, setPatientHour] = useState("0")
 
     const params = useParams()
     const id = params.id
@@ -16,9 +20,7 @@ export default function Retard(props) {
         id: id,
         }).then((data) => {
         setUser(data.data[0])
-        // console.log(data.data[0])
         setLoaded(true)
-        setDelay(data.data[0].delay)
         });
     }
 
@@ -35,12 +37,22 @@ export default function Retard(props) {
     //     setLoaded(true)
     //     });
     // }
+
+    function getDoctorDelay(e){
+        var patientAppointment = e.target.value;
+        setPatientHour(e.target.value);
+        if (patientAppointment <= user.endDelay){
+            setDelay(user.delay);
+        } else{
+            setDelay(0)
+        }
+    }
     
     if (props.page === "doctor" && loaded){
         return (
             <div className="retard-doctor">
                 <p>Retard annoncé:</p>
-                <select className="dropdown" value={delay} onChange={(e) => setDelay(e.target.value)}>
+                <select className="dropdown" value={user.delay} onChange={(e) => setDelay(e.target.value)}>
                     <option value="0">Pas de retard</option>
                     <option value="15">15 minutes</option>
                     <option value="30">30 minutes</option>
@@ -55,18 +67,20 @@ export default function Retard(props) {
             <div>
                 <div className="heure-patient">
                     <p>Heure de votre rendez-vous:</p>
-                    <select className="dropdown">
-                        <option value="0">15h00</option>
-                        <option value="15">16h00</option>
-                        <option value="30">17h00</option>
-                        <option value="45">18h00</option>
-                        <option value="60">19h00</option>
+                    <select className="dropdown" onChange={getDoctorDelay}>
+                        <option value="0"></option>
+                        <option value={hour}>{hour}h00</option>
+                        <option value={hour+1}>{hour+1}h00</option>
+                        <option value={hour+2}>{hour+2}h00</option>
+                        <option value={hour+3}>{hour+3}h00</option>
                     </select>
                 </div>
+                { patientHour !== "0" && 
                 <div className="retard-patient">
                     <p>Retard annoncé:</p>
-                    <p id="retard-announced">{user.delay} minute(s)</p>
+                    <p id="retard-announced">{delay} minute{delay>0 && "(s)"}</p>
                 </div>
+                }
             </div>
         )
     }
