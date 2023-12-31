0b45a62e86a1cb6cea18be8980d7dfc3482a80be
Harold-coder <hjc2154@columbia.edu>
2023-12-31 14:51:36 +0100

    Fixed navbar when logged in

--- src/components/HomePage/NavbarHome.js
+++ src/components/HomePage/NavbarHome.js
@@ -1,13 +1,42 @@
-import React from "react"
-import { Link } from "react-router-dom"
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { jwtDecode } from "jwt-decode";
 
 export default function NavbarHome() {
+    const navigate = useNavigate();
+    const token = localStorage.getItem('token');
+    let doctorId;
+
+    // Check if the user is logged in by decoding the token
+    if (token) {
+        try {
+            const decodedToken = jwtDecode(token);
+            doctorId = decodedToken.doctor_id;
+            // Check if the token has expired
+            const current_time = Date.now() / 1000;
+            if (decodedToken.exp < current_time) {
+                // Token has expired, navigate to login
+                navigate('/login');
+            }
+        } catch (error) {
+            console.error("Error decoding token:", error);
+            navigate('/login');
+        }
+    }
+
     return (
-        
         <nav>
-            <Link className="link" to="/login">
-                <button className="inscription">Médecin</button>
-            </Link>
+            {doctorId ? (
+                // If doctorId is present, show profile link
+                <Link className="link" to={`/doctorPage/${doctorId}`}>
+                    <button className="inscription">Mon Profil</button>
+                </Link>
+            ) : (
+                // Otherwise, show the login button
+                <Link className="link" to="/login">
+                    <button className="inscription">Médecin</button>
+                </Link>
+            )}
         </nav>
-    )
+    );
 }
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,15 +1,12 @@
 import { Link } from 'react-router-dom';
 import Search from '../components/HomePage/Search';
 import React from 'react';
+import NavbarHome from '../components/HomePage/NavbarHome';
 
 function HomePage() {
   return (
     <div className='home-page'>
-      <nav>
-            <Link className="link" to="/login">
-                <button className="inscription">Médecin</button>
-            </Link>
-        </nav>
+        <NavbarHome />
         <h1 className="hero"><b id="first-letter">U</b>nilate</h1>
       <Search/>
     </div>
