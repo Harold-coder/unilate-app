9c092e327c83eba64dd1933fba8debe56a68e585
Harold-coder <hjc2154@columbia.edu>
2024-01-07 20:46:20 -0500

    Added an animation on the Hero

--- src/pages/HomePage/Hero.js
+++ src/pages/HomePage/Hero.js
@@ -2,6 +2,14 @@ import React from "react"
 
 export default function Hero() {
     return (
-        <h1 className="hero"><b id="first-letter">U</b>nilate</h1>
+        <h1 className="hero">
+            <span className="hero-letter" id="first-letter" style={{animationDelay: '0s'}}>U</span>
+            <span className="hero-letter" style={{animationDelay: '0.1s'}}>n</span>
+            <span className="hero-letter" style={{animationDelay: '0.2s'}}>i</span>
+            <span className="hero-letter" style={{animationDelay: '0.3s'}}>l</span>
+            <span className="hero-letter" style={{animationDelay: '0.4s'}}>a</span>
+            <span className="hero-letter" style={{animationDelay: '0.5s'}}>t</span>
+            <span className="hero-letter" style={{animationDelay: '0.6s'}}>e</span>
+        </h1>
     )
 }
