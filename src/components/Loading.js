2c653290e6ef9f0afc075b2bc82f18a3d83381ea
Harold-coder <hjc2154@columbia.edu>
2024-01-07 19:50:05 -0500

    Loading component is now sick

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,7 +1,11 @@
-import React from "react"
+import React from "react";
+import "../Loading.css"; // Make sure to import the CSS file
 
 export default function Loading() {
     return (
-        <div className="loading">En cours de chargement...</div>
-    )
+        <div className="loading-container">
+            <div className="loading-bar"></div>
+            <div className="loading-text">En cours de chargement...</div>
+        </div>
+    );
 }
