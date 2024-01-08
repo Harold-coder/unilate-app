85dbbe8fd9546da6faec2780cad0bc235ef24469
Harold-coder <hjc2154@columbia.edu>
2024-01-07 20:57:14 -0500

    Modified the animation a little

--- src/pages/HomePage/Hero.js
+++ src/pages/HomePage/Hero.js
@@ -4,12 +4,12 @@ export default function Hero() {
     return (
         <h1 className="hero">
             <span className="hero-letter" id="first-letter" style={{animationDelay: '0s'}}>U</span>
-            <span className="hero-letter" style={{animationDelay: '0.1s'}}>n</span>
-            <span className="hero-letter" style={{animationDelay: '0.2s'}}>i</span>
-            <span className="hero-letter" style={{animationDelay: '0.3s'}}>l</span>
-            <span className="hero-letter" style={{animationDelay: '0.4s'}}>a</span>
-            <span className="hero-letter" style={{animationDelay: '0.5s'}}>t</span>
-            <span className="hero-letter" style={{animationDelay: '0.6s'}}>e</span>
+            <span className="hero-letter" style={{animationDelay: '0.2s'}}>n</span>
+            <span className="hero-letter" style={{animationDelay: '0.4s'}}>i</span>
+            <span className="hero-letter" style={{animationDelay: '0.6s'}}>l</span>
+            <span className="hero-letter" style={{animationDelay: '0.8s'}}>a</span>
+            <span className="hero-letter" style={{animationDelay: '1s'}}>t</span>
+            <span className="hero-letter" style={{animationDelay: '1.2s'}}>e</span>
         </h1>
     )
 }
